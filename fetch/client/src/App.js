ccd6bb75122ebd11c72a8ea1acff2ba7af0e64ce
Hyeyeon(Anne) <khy2106@naver.com>
2022-09-27 22:58:10 +0900

    CORS error 해결, 리렌더링 방지 위한 useEffect

--- fetch/client/src/App.js
+++ fetch/client/src/App.js
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  
-  fetch('http://localhost:4500/api/todo').then((res) => res.json()).then((data) => console.log(data))
+  const [todoList, setTodoList] = useState([]);
+  useEffect(()=> {
+    fetch('http://localhost:4500/api/todo').then((res) => res.json()).then((data) => setTodoList(data));
+  },[])
   return (
     <div className="App">
-      Hi!
+      {todoList.map((todo) => (
+        <div key={todo.id}>
+          <div>{todo.id}</div>
+          <div>{todo.text}</div>
+          <div>{todo.done ? "y" : "n"}</div>
+        </div>
+      ))}
     </div>
   );
 }
